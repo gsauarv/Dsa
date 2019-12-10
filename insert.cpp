0f5f13574946ee7e544a8348ab2b03de23176262
gsauarv <gsaurav2000@gmail.com>
2019-12-10 08:19:08 +0545

    Added Delete Function for Node Deletion

--- insert.cpp
+++ insert.cpp
@@ -2,20 +2,19 @@
 #include<conio.h>
 using namespace std;
 struct Node
-{
-	int data;
+{	int data;
 	struct Node *next;
 };
 struct Node *head=NULL;
 void insert(int data ,int pos)
 {
-	Node *temp = new(struct Node);
-	temp->data = data;
-	temp ->next = NULL;
+	Node *temp1 = new(struct Node);
+	temp1->data = data;
+	temp1->next = NULL;
 	if(pos==1)
 	{
-		temp->next=head;
-		head=temp;
+		temp1->next=head;
+		head=temp1;
 		return;
 	}
 	Node *temp2 =head;
@@ -23,18 +22,33 @@ void insert(int data ,int pos)
 	{
 		temp2=temp2->next;
 	}
-	temp->next=temp2->next;
-	temp2->next=temp;
+	temp1->next=temp2->next;
+	temp2->next=temp1;
 	
 	
 }
+
+void delete_n(int pos)
+{
+	Node *temp1 = head;
+	if(pos==1)
+	{
+		head  = temp1->next;
+		delete temp1;
+		return;		
+	}	
+	for(int i=0;i<pos-2;i++)
+	temp1=temp1->next;
+	Node *temp2=temp1->next;
+	delete temp2;
+}
 void display()
 {
-	struct Node *temp = head;
-	while(temp!=NULL)
+	struct Node *temp1 = head;
+	while(temp1!=NULL)
 	{
-		cout<<temp->data<<"->";
-		temp=temp->next;
+		cout<<temp1->data<<"->";
+		temp1=temp1->next;
 
 	}
 }
@@ -43,7 +57,7 @@ int main()
 	int data,pos,choice;
 	do{
 	
-	cout<<"\nEnter your choice\n 1:Insert \t 2:Display \n 3:Exit\n";
+	cout<<"\nEnter your choice\n 1:Insert \t 2:Display \n 3:Delete \t4:Exit";
 	cin>>choice;
 	switch(choice)
 	{
@@ -59,13 +73,21 @@ int main()
 			display();
 			break;
 		case 3:
+			cout<<"Enter the position to delete";
+			cin>>pos;
+			delete_n(pos);
+			cout<<(pos)+" Node Deleted";
 			break;
+			
+		case 4:
+			break;
+			
 		default:
 			cout<<"\nEnter the valid input\n";
 		
 	}
 	
 	
-}while(choice!=3);
+}while(choice!=4);
 }
 
